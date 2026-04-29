0fc4f850de169354c1817bc641f7ce158437b38b
Gabriel Lopes Parreira dos Santos <gabriel.lopessb@gmail.com>
2026-04-29 17:16:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_service():
     if not creds:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
+        creds = flow.run_local_server(port=0, open_browser=False)
 
         with open('token.pkl', 'wb') as token:
             pickle.dump(creds, token)
